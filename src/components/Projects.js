2bc60de5a98277044f8e3384fd6debd79fd73588
Vignesh Rajasekar <127473360+vichusekar@users.noreply.github.com>
2024-02-06 01:24:34 -0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,25 +14,29 @@ function Projects() {
       name: 'Gmail Clone',
       description: 'This App developed for gmail users who have Gmail and work in authentication',
       img: gmail,
-      deployurl: 'https://whimsical-malabi-870032.netlify.app/'
+      deployurl: 'https://whimsical-malabi-870032.netlify.app/',
+      githuburl: 'https://github.com/vichusekar/Gmail-Clone-Client'
     },
     {
       name: 'Amazon Web Scraping',
       description: 'The Amazon web scraping used for scraping the products from amazon and take specific requirements about products',
       img: amazon,
-      deployurl: 'https://sparkly-clafoutis-d90c6d.netlify.app/'
+      deployurl: 'https://sparkly-clafoutis-d90c6d.netlify.app/',
+      githuburl: 'https://github.com/vichusekar/WebScraping-Ecommerce/tree/clientt'
     },
     {
       name: 'URL Shortener',
       description: 'The URL Shortener app will convert the long url into small url',
       img: Url,
-      deployurl: 'https://subtle-sorbet-a3f946.netlify.app/'
+      deployurl: 'https://subtle-sorbet-a3f946.netlify.app/',
+      githuburl: 'https://github.com/vichusekar/urlshortener-frontend'
     },
     {
       name: 'Password Reset',
       description: 'The password reset web application use the user can reset password only become a already user',
       img: password,
-      deployurl: 'https://capable-bienenstitch-f6d4b0.netlify.app/'
+      deployurl: 'https://capable-bienenstitch-f6d4b0.netlify.app/',
+      githuburl: 'https://github.com/vichusekar/password-reset'
     }
   ])
   return <>
@@ -50,13 +54,22 @@ function Projects() {
                 {e.description}
               </Card.Text>
             </Card.Body>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+            <a
+              href={e.githuburl}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Button variant='secondary'><i className="fa-brands fa-1x fa-github"></i>source</Button>
+            </a>
             <a
               href={e.deployurl}
               target='_blank'
               rel='noreferrer'
             >
-              <Button style={{ marginLeft: '100px' }}><i class="fa-solid fa-arrow-up-right-from-square"></i>site</Button>
+              <Button style={{ marginLeft: '0px' }}><i class="fa-solid fa-arrow-up-right-from-square"></i>site</Button>
             </a>
+            </div>
           </Card>
 
         })
