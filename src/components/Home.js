84b165121eb1fa6cd6c16f47d537375336250afa
Vignesh Rajasekar <127473360+vichusekar@users.noreply.github.com>
2024-03-26 12:19:04 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Header from '../components/Header'
-import Resume from '../Pdf/Vignesh Rajasekar .pdf'
+import Resume from '../Pdf/Vignesh's Resume.pdf'
 
 function Home() {
   return <>
